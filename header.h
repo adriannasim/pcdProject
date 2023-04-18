1da904fe17f0205cae20b8a8bd6f341d47badc44
adriannasim <adriannasim@gmail.com>
2023-04-18 16:26:43 +0800

    menu and header file

---
+++ header.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <Windows.h>
+#pragma warning (disable: 4996)
---
+++ menu.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+void main() {
+    printf("WELCOME TO TWAY MLM SYSTEM.\n");
+    printf("Please choose MEMBER or STAFF login: \n");
+    printf("1. Member\n");
+    printf("2. Staff\n");
+    scanf("%d", &menuChoice)
+}
