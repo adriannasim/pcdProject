2bffb7ac4463213840a520b15105f0485b5f6db2
adriannasim <adriannasim@gmail.com>
2023-04-23 02:51:26 +0800

    change details not done

--- assignment/memberModule.c
+++ assignment/memberModule.c
@@ -19,13 +19,13 @@ void memberModule(char username[21]) {
             viewInfo(username);
             break;
         case 2:
-            purchase();
+            //purchase();
             break;
         case 3:
-            pHistory();
+            //pHistory();
             break;
         case 4:
-            viewRef();
+            viewRef(username);
             break;
         default:
             printf("Invalid choice. Please enter again.\n");
@@ -34,11 +34,10 @@ void memberModule(char username[21]) {
 }
 
 void viewInfo(char username[21]) {
-    int option;
-    mDetails viewDetails;
-    FILE* flogin = fopen("memberLogin.txt", "r");
-    FILE* fdetails = fopen("memberLogin.txt", "a");
-    if (flogin == NULL || fdetails == NULL) {
+    int option, chg;
+    mDetails viewDetails, chgDetails;
+    FILE* fdetails = fopen("memberDetails.txt", "a+");
+    if (fdetails == NULL) {
         printf("Error in opening file.\n");
         return;
     }
@@ -48,27 +47,79 @@ void viewInfo(char username[21]) {
             printf("%-24s%-24s%-10s%-16s%-12s\n", "USERNAME", "NAME", "GENDER", "IC", "PHONE NO.");
             printf("%-24s%-24s%-10s%-16s%-12s\n", "========", "====", "======", "==", "=========");
             printf("%-24s%-24s%-10c%-16s%-12s\n\n\n", username, viewDetails.name, viewDetails.gender, viewDetails.ic, viewDetails.phoneNo);
-            while (1) {
-                printf("1. BACK\n");
-                printf("2. EDIT DETAILS\n");
-                printf("3. CHANGE PASSWORD\n");
+        }
+    } 
+    fclose(fdetails);
+    while (1) {
+        printf("1. BACK\n");
+        printf("2. EDIT DETAILS\n");
+        printf("3. CHANGE PASSWORD\n");
+        printf("> ");
+        scanf("%d", &option);
+        rewind(stdin);
+        switch (option) {
+        case 1:
+            return;
+            break;
+        case 2:
+            //incase customer does not change any details
+            strcpy(chgDetails.name, viewDetails.name);
+            chgDetails.gender = viewDetails.gender;
+            strcpy(chgDetails.ic, viewDetails.ic);
+            strcpy(chgDetails.phoneNo, viewDetails.phoneNo);
+            while (1) { 
+                printf("\nCHOOSE OPTION TO CHANGE DETAILS\n");
+                printf("===============================\n");
+                printf("1. NAME\n");
+                printf("2. GENDER\n");
+                printf("3. IC\n");
+                printf("4. PHONE NO.\n");
+                printf("5. DONE\n");
                 printf("> ");
-                scanf("%d", &option);
+                scanf("%d", &chg);
                 rewind(stdin);
-                switch (option) {
+                switch (chg) {
                 case 1:
-                    return;
+                    printf("Enter NAME: ");
+                    scanf("%[^\n]", &chgDetails.name);
+                    rewind(stdin);
                     break;
                 case 2:
-                    editDetails();
+                    printf("Enter GENDER (M or F): ");
+                    scanf("%c", &chgDetails.gender);
+                    rewind(stdin);
+                    toupper(chgDetails.gender);
+                    if (chgDetails.gender != 'M' || chgDetails.gender != 'F') {
+                        printf("Wrong input. Please try again.");
+                        chgDetails.gender = viewDetails.gender;
+                    }
                     break;
                 case 3:
-                    chgPsw();
+                    printf("Enter IC: ");
+                    scanf("%[^\n]", &chgDetails.ic);
+                    rewind(stdin);
+                    break;
+                case 4:
+                    printf("Enter PHONE NO: ");
+                    scanf("%[^\n]", &chgDetails.phoneNo);
+                    rewind(stdin);
+                    break;
+                case 5:
+                    while (fscanf(fdetails, "%[^|]|%[^|]|%[^|]|%[^|]|%[^\n]\n", &viewDetails.lDetails.username, &viewDetails.name, &viewDetails.gender, &viewDetails.ic, &viewDetails.phoneNo) != EOF) {
+                        if (viewDetails.lDetails.username)
+                    }
+                        fprintf
+                    return;
                     break;
                 default:
                     printf("Invalid choice. Please enter again.\n");
                 }
             }
+        case 3:
+            chgPsw();
+            break;
+        default:
+            printf("Invalid choice. Please enter again.\n");
         }
     }
 }
@@ -77,6 +128,91 @@ void viewInfo(char username[21]) {
 
 //void pHistory()
 
-void viewRef() {
+void viewRef(char username[21]) {
+    char referrer[21], referredUser[21], addRef[21];
+    int no = 0, option;
+    FILE* fref, *flogin;
+    Login check;
 
+    printf("\nVIEW REFERRALS\n");
+    printf("==============\n");
+
+    while (1) {
+        //view referrals menu
+        printf("Please choose your option: \n");
+        printf("1. Add referrer\n");
+        printf("2. View referrals\n");
+        printf("3. Exit\n");
+        printf("> ");
+        scanf("%d", &option);
+        rewind(stdin);
+        switch (option) {
+        case 1:
+            //add referrer
+            fref = fopen("referrals.txt", "a+");
+            if (fref == NULL) {
+                printf("Error in opening file.\n");
+                return;
+            }
+            //check if user has already referred to a user
+            while (fscanf(fref, "%[^|]|%[^\n]\n", &referrer, &referredUser) != EOF) {
+                if (strcmp(referredUser, username) == 0) {
+                    printf("You already have a referrer!\n");
+                    break;
+                }
+            }
+            //if user has no referrer:
+            printf("Please enter referral USERNAME:\n");
+            printf("> ");
+            scanf("%[^\n]", &addRef);
+            rewind(stdin);
+            //check if entered username is in the system
+            flogin = fopen("memberLogin.txt", "r");
+            if (flogin == NULL) {
+                printf("Error in opening file.\n");
+                return;
+            }
+            while (fscanf(flogin, "%[^|]|%[^\n]\n", &check.username, &check.password) != EOF) {
+                //yes, username is in the system
+                if (strcmp(addRef, check.username) == 0) {
+                    fprintf(fref, "%s|%s\n", addRef, username);
+                    printf("Referral Added.\n");
+                    return;
+                }
+            }
+            //no username is not in the system
+            printf("Username is not in the system. Please try again.\n");
+            fclose(fref);
+            fclose(flogin);
+            break;
+        case 2:
+            //view referrals
+            fref = fopen("referrals.txt", "a");
+            if (fref == NULL) {
+                printf("Error in opening file.\n");
+                return;
+            }
+
+            printf("\nREFERRALS:\n\n");
+            printf("%-6s%-24s\n", "NO", "USERNAME");
+            printf("%-6s%-24s\n", "==", "========");
+            while (fscanf(fref, "%[^|]|%[^\n]\n", &referrer, &referredUser) != EOF) {
+                if (strcmp(username, referrer) == 0) {
+                    no++;
+                    printf("%-6d%-24s\n", no, referredUser);
+                }
+            }
+            fclose(fref);
+            break;
+        case 3:
+            //exit
+            return;
+            break;
+        default:
+            //invalid choice
+            printf("Invalid choice. Please enter again.\n");
+        }
+    }
+
+    
 }
